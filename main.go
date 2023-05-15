6064c3d62ae0e21e742fda5fe9a10c4d9e20946d
René Delion <rene.leon.delion@gmail.com>
2023-05-15 21:08:19 +0100

    Add userInput function

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
 
 /*
  +----+
@@ -61,12 +68,52 @@ var hangmanArr = [7]string{
 		"     ===\n",
 }
 
+var wordArr = [7]string{
+	"JAZZ", "ZODIAC", "ZIPPER", "FLUFF", "ZOMBIE", "APPLE", "TREE",
+}
+
+var randWord string
+var guessedLetters string
+var correctLetter []string
+var wrongGuesses []string
+
+func getLetterInput() rune {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	rInputSlice := []rune(input)
+	if len(rInputSlice) > 1 {
+		fmt.Printf("Please Enter Only One Letter: ")
+		return getLetterInput()
+	}
+	if !unicode.IsLetter(rInputSlice[0]) {
+		fmt.Printf("Please Enter a Letter: ")
+		return getLetterInput()
+	}
+	return rInputSlice[0]
+}
+
 func main() {
-	fmt.Print(hangmanArr[0])
-	fmt.Print(hangmanArr[1])
-	fmt.Print(hangmanArr[2])
-	fmt.Print(hangmanArr[3])
-	fmt.Print(hangmanArr[4])
-	fmt.Print(hangmanArr[5])
-	fmt.Print(hangmanArr[6])
+	var lives int = 6
+	// Show Game Board
+
+	// Get a letter from the user
+
+	// A. If they guesses letter in word
+	// Add to correctLetter
+	// 1. Are there more letters to guess?
+	// 2. If no more letters to guess (You Win)
+	// B. If they guessed letter not in word
+	// 1. Add new letter to guessedLetters,
+	// wrongGuesses
+	// Check if they died
+
+	for lives > 0 {
+		fmt.Println(hangmanArr[lives])
+		fmt.Printf("Please Guess a Letter: ")
+		letter := getLetterInput()
+	}
 }
